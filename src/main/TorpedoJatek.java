1d038cd8ccdc22526d01808961b0804f470fd1cf
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-05-16 14:56:04 +0200

    alapkód

---
+++ src/main/TorpedoJatek.java
@@ -0,0 +1,32 @@
+
+package main;
+
+
+public class TorpedoJatek {
+
+    private int[] mezok;
+    private int[] hajok;
+    private int tipp;
+
+    public TorpedoJatek(int[] mezok, int[] hajok, int tipp) {
+        this.mezok = mezok;
+        this.hajok = hajok;
+        this.tipp = tipp;
+    }
+    
+    public void mezokFeltolt(){
+        
+    }
+    
+    public void hajokElhelyez(){
+        
+    }
+    
+    public void tippTorpedo(){
+        
+    }
+    
+    public boolean tippHajokEll(){
+        return true;
+    }
+}
